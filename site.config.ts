a0c319a83086955e85c3ba6c076316b3517ae089
handsomeRobot <kangtboy@gmail.com>
2023-01-27 23:33:25 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,6 +17,7 @@ export default siteConfig({
   //description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
+  email: 'kang.xu92@outlook.com',
   //twitter: 'transitive_bs',
   //github: 'transitive-bullshit',
   //linkedin: 'fisch2',
